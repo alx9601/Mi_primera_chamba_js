c1b03725254c913d695e18909ddedce441b52e1b
alx9601 <fer_alexei196@hotmail.com>
2024-03-12 09:34:45 -0500

    mi primera confirmación de actualización

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let contenidoTitulo="Nombre";
+let contenidoTitulo="Mi web";
 
 let titulo = document.querySelector(".logo .nombre");
 titulo.innerHTML = contenidoTitulo;
@@ -17,21 +17,22 @@ if(textoTitulo == "Nombre"){
 // FUNCIONES
 
 let nombre = "Fernando";
+let pais = "Perú";
 let ciudad = "Lima";
-let gusto = "chocolate";
+let gusto = "ceviche";
 
 let parrafo = document.querySelector(".parrafo2");
 
-function cambiarTexto(nombre, ciudad, gusto){
-    let contenido = `Me llamo ${nombre}, naci en ${ciudad} y vivo 
-    en Caballito. Me gusta el ${gusto} y salir a pasear en dias de sol. 
+function cambiarTexto(nombre, pais, ciudad, gusto){
+    let contenido = `Me llamo ${nombre}, soy de ${pais} y vivo 
+    en ${ciudad}. Me gusta el ${gusto} y salir a pasear en dias de sol. 
     Me encantaría aprender a programar para poder ayudar a 
     las personas a mostrar lo que hacen.`;
 
     return contenido;
 }
 
-parrafo.innerText = cambiarTexto(nombre,ciudad,gusto);
+parrafo.innerText = cambiarTexto(nombre,pais,ciudad,gusto);
 
 //MENU NAVEGACION
 
@@ -41,3 +42,36 @@ menu_responsive.onclick = function () {
   navBar = document.querySelector(".navbar");
   navBar.classList.toggle("active");
 };
+
+//FORMULARIO
+
+const form = document.getElementById("form");
+const nameform = document.getElementById("nombre");
+const textform = document.getElementById("alertas");
+
+function validarFormulario() {
+  let warnings = "";
+  let valido = true;
+  textform.innerHTML = "";
+
+  if (nameform.value.length < 4) {
+    warnings += `El nombre debe contener más de 4 carcateres`;
+    valido = false;
+  }
+
+  if (!valido) {
+    textform.innerHTML = warnings;
+  } else {
+    textform.innerHTML = "Enviado";
+  }
+  return valido;
+}
+
+form.addEventListener("submit", (e) => {
+  if (validarFormulario()) {
+    // Si la validación es exitosa, puedes enviar el formulario
+    formulario.submit();
+  } else {
+    e.preventDefault(); // Evita que el formulario se envíe automáticamente
+  }
+});
